c16563c055f2a3078053693d45116ea594b3c759
Sovichea TEP <sovichea.tep@gmail.com>
2023-03-23 15:13:00 +0700

    update user settings

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,36 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 80,
+      disabled_filetypes = { "help", "text", "markdown" },
+      custom_colorcolumn = {},
+      scope = "file",
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
 }
