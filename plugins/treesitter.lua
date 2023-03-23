fc3b569ef67a6e9b250ee6f976d94091a387b067
Sovichea TEP <sovichea.tep@gmail.com>
2023-03-23 14:45:26 +0700

    migrate user setting to v3.0

---
+++ plugins/treesitter.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "c", "vim", "devicetree", "cmake", "yaml" },
+  },
+}
